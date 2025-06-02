fadaf3fc06390386131f01506f5bbe02797a0a43
biology8862 <biology8862@gmail.com>
2025-06-02 10:59:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "25586552"
-api_hash = "f265cba9d76dc6ad70914accbe758f47"
+api_id = "28585138"
+api_hash = "74b7afde7d9a546db3451cf4689f4e14"
 bot_token = ""
 auth_users = [1368753935,938905233,7813618750]
